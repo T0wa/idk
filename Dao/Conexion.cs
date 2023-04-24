6706c8dc4ab8d761c6b89e2c0cc03355f59fa7a3
T0wa <86084007+T0wa@users.noreply.github.com>
2023-04-24 19:44:06 +0200

    Add files via upload

---
+++ Dao/Conexion.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Dao
+{
+    public class Conexion
+    {
+        private readonly string ruta = "Data Source=localhost\\sqlexpress;Initial Catalog=loging;Integrated Security=True";
+
+
+        public SqlConnection ObtenerConexion()
+        {
+            SqlConnection cn = new SqlConnection(ruta);
+            try
+            {
+                cn.Open();
+                return cn;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public DataTable obtenerTabla(string consulta)
+        {
+            SqlConnection cn = ObtenerConexion();
+            SqlDataAdapter adp = new SqlDataAdapter(consulta, cn);
+            DataSet ds = new DataSet();
+            adp.Fill(ds, "Tabla");
+            cn.Close();
+
+            return ds.Tables["Tabla"];
+        }
+
+        public bool insertarUsuario(string consulta)
+        {
+            try
+            {
+                SqlConnection cn = ObtenerConexion();
+                SqlCommand cmd = new SqlCommand(consulta, cn);
+                int i = cmd.ExecuteNonQuery();
+
+                cn.Close();
+                if (i > 0)
+                {
+                    return true;
+                }
+            }
+            catch (SqlException ex)
+            {
+
+            }
+
+
+            return false;
+        }
+
+
+        public bool updatePP(string consulta)
+        {
+            SqlConnection cn = ObtenerConexion();
+            SqlCommand cmd = new SqlCommand(consulta, cn);
+            int i = cmd.ExecuteNonQuery();
+
+            cn.Close();
+            if (i > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+}
---
+++ Dao/usuarioDao.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Entidades;
+
+
+namespace Dao
+{
+    public class usuarioDao
+    {
+
+        Conexion con = new Conexion();
+        public bool existeUsuario(usuario user)
+        {
+
+            string consulta = "select * from users where userName ='" + user.NombreUsuario + "' AND password = '" + user.Contraseña + "'";
+
+
+            if (con.obtenerTabla(consulta).Rows.Count > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+        public bool insertarUsuario(usuario user)
+        {
+            string consulta = "insert into users (userName,password,email) VALUES('" + user.NombreUsuario + "' , '" + user.Contraseña + "' , '" + user.Mail + "')";
+
+            return con.insertarUsuario(consulta);
+        }
+
+        public usuario cargarSession(usuario user)
+        {
+            DataTable tablaUsuairos = new DataTable();
+            string consulta = "select * from users where userName ='" + user.NombreUsuario + "' AND password = '" + user.Contraseña + "'";
+
+            tablaUsuairos = con.obtenerTabla(consulta);
+
+            user.NombreUsuario = tablaUsuairos.Rows[0][0].ToString();
+            user.Contraseña = tablaUsuairos.Rows[0][1].ToString();
+            user.Mail = tablaUsuairos.Rows[0][2].ToString();
+            user.Estado = tablaUsuairos.Rows[0][3].ToString();
+            user.Pp = tablaUsuairos.Rows[0][4].ToString();
+
+            return user;
+        }
+
+        public bool cargarPP(string pp, usuario user)
+        {
+            string consulta = "update users set pp = '" + pp + "' where userName = '"+ user.NombreUsuario + "' ";
+
+            return con.updatePP(consulta);
+        }
+
+
+    }
+}
