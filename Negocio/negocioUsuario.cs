81c4d350a8c6a85be6cb0d6a35be9f7111699947
T0wa <86084007+T0wa@users.noreply.github.com>
2023-04-24 19:49:38 +0200

    Add files via upload

---
+++ Negocio/negocioUsuario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dao;
+using Entidades;
+
+
+namespace Negocio
+{
+    public class  negocioUsuario
+    {
+
+        private usuarioDao userDao = new usuarioDao();
+
+        public bool registrarse(usuario user)
+        {
+            return userDao.insertarUsuario(user);
+
+        }
+
+        public bool existeUsuario(usuario user)
+        {
+            return userDao.existeUsuario(user);
+        }
+
+        public usuario cargarSession(usuario user)
+        {
+            return userDao.cargarSession(user);
+        }
+
+        public bool cargarPP(string pp,usuario user)
+        {
+
+            return userDao.cargarPP(pp, user);
+            
+        }
+
+    }
+}
